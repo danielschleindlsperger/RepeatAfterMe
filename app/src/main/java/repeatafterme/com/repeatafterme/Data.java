af28bb81c100d8459e1da8542041bfb72b3279ef
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2015-12-06 18:09:27 +0100

    Added some speech data sets for testing

--- app/src/main/java/repeatafterme/com/repeatafterme/Data.java
+++ app/src/main/java/repeatafterme/com/repeatafterme/Data.java
@@ -33,18 +33,35 @@ public class Data {
         ListenAndRepeat.add("Test");
         ListenAndRepeat.add("Water");
         ListenAndRepeat.add("House");
-        ListenAndRepeat.add("Desk");
-        ListenAndRepeat.add("Tree");
+        ListenAndRepeat.add("The sixth sick sheik's sixth sheep's sick.");
+        ListenAndRepeat.add("I want to be a juror on a rural brewery robbery case.");
+        ListenAndRepeat.add("How many boards\n" +
+                "Could the Mongols hoard\n" +
+                "If the Mongol hordes got bored?");
+        ListenAndRepeat.add("How can a clam cram in a clean cream can?");
+        ListenAndRepeat.add("The thirty-three thieves thought that they thrilled the throne throughout Thursday.");
+        ListenAndRepeat.add("Can you can a can as a canner can can a can?");
+
+
+
     }
 
     // Add entries for Read and Repeat game mode
     private void initReadAndRepeat(){
-        ListenAndRepeat.add("Hello");
-        ListenAndRepeat.add("Test");
-        ListenAndRepeat.add("Water");
-        ListenAndRepeat.add("House");
-        ListenAndRepeat.add("Desk");
-        ListenAndRepeat.add("Tree");
+        ReadAndRepeat.add("Hello");
+        ReadAndRepeat.add("Test");
+        ReadAndRepeat.add("Water");
+        ReadAndRepeat.add("House");
+        ReadAndRepeat.add("Desk");
+        ReadAndRepeat.add("Tree");
+        ReadAndRepeat.add("Seth at Sainsbury's sells thick socks.");
+        ReadAndRepeat.add("Roberta ran rings around the Roman ruins.");
+        ReadAndRepeat.add("Stupid superstition!");
+        ReadAndRepeat.add("There was a fisherman named Fisher\n" +
+                "who fished for some fish in a fissure.\n" +
+                "Till a fish with a grin,\n" +
+                "pulled the fisherman in.\n" +
+                "Now they're fishing the fissure for Fisher.");
     }
 
 
