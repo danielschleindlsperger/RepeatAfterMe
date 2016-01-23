1f987436df535e23bd5d3552ea01dace26df8000
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2016-01-23 21:03:57 +0100

    added javadoc, fixed some indentation

--- app/src/main/java/repeatafterme/com/repeatafterme/MainActivity.java
+++ app/src/main/java/repeatafterme/com/repeatafterme/MainActivity.java
@@ -3,23 +3,20 @@ package repeatafterme.com.repeatafterme;
 import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-/*
-This class represents the home screen view and all of its logic.
-
+/**
+ *
+This class represents the entry point to the application. It also represents the home screen view and some of its view based logic.
+ <p>On the first start it shows a login screen. After successful validation the main activity view is loaded.
+ After completion of a game mode the main activity view is loaded also.</p>
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // Set view to main instead of login based on intent extra
+        /**
+         * Intent to determine if main view or login view is to be loaded
+         */
         Intent viewIntent = getIntent();
         String view = viewIntent.getStringExtra("VIEW");
 
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Hide Android keyboard if no view in focus
+    /** Hide Android keyboard if no view in focus*/
     private void hideKeyboard() {
         View view = getCurrentFocus();
         if (view != null) {
@@ -82,24 +82,62 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Username validation
+    /**
+     * <h5>
+     *     Validates the user
+     * </h5>
+     * <p>
+     *     Currently the validation is only based on the length of the user name. More advanced validation can be added here.
+     * </p>
+     *
+     * @param user(required) username that was entered in the login form
+     * @return boolean of (un)successful validation
+     */
     public boolean validateUser(String user) {
         return user.length() > 5;
         //TODO Add more validation logic
     }
 
-    // Password validation
+    /**
+     * <h5>
+     *     Validates the password
+     * </h5>
+     * <p>
+     *     Currently the validation is only based on the length of the password. More advanced validation can be added here.
+     * </p>
+     *
+     * @param password(required) password that was entered in the login form
+     * @return boolean of successful validation
+     */
     public boolean validatePassword(String password) {
         return password.length() > 5;
     }
 
-    // Make individual welcome message
+    /**
+     * <h5>
+     *     Generate individual welcome message
+     * </h5>
+     * <p>
+     *     Takes the username from the input form and generates a greetings message which is placed on the home view of the app.
+     * </p>
+     *
+     * @param user(required) username that was entered in the login form
+     */
     private void welcomeMessage(String user) {
         final TextView welcomeText = (TextView) findViewById(R.id.text_welcome);
         welcomeText.setText(
                 "Hallo " + user + "! Was willst du machen?");
     }
 
+    /**
+     * <h5>
+     *     Login user
+     * </h5>
+     * <p>
+     *     This Method is part of the login process. It generates a toast to indicate the success of the login process and calls another function to set up the main view.
+     * </p>
+     *
+     */
     private void doLogin(){
         Toast.makeText(getApplicationContext(), "Login erfolgreich!", Toast.LENGTH_SHORT).show();
 
@@ -107,6 +145,15 @@ public class MainActivity extends AppCompatActivity {
         createMainView();
     }
 
+    /**
+     * <h5>
+     *     Set up main view
+     * </h5>
+     * <p>
+     *     This Method is part of the login process. It sets the view to the layout of the main activity.
+     * </p>
+     *
+     */
     public void createMainView(){
 
         // Set view to main activity view
@@ -115,9 +162,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Start a game mode activity based on selection
+
+    /**
+     *  <h5>
+     *      Start a game mode
+     *  </h5>
+     *  <p>
+     *      Gets the game mode from the supplied view's tag and starts the corresponding game mode.
+     *  </p>
+     * @param v(required)   View element with tag which describes the game mode to be started
+     */
     public void startGame(View v){
         String mode = v.getTag().toString();
-        // Toast.makeText(getApplicationContext(), mode, Toast.LENGTH_SHORT).show();
+
         Log.d("Mode selected", mode);
 
         if (mode.equalsIgnoreCase("ListenAndRepeat")){
